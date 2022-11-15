e11a7221de9b57f15d6894981fb7d1d982769dd0
frariqsan <frariqsan@gmail.com>
2022-11-16 00:02:39 +0100

    First Commit

---
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -0,0 +1,19 @@
+package proyectofuturo;
+public class ProyectoFuturo {
+
+    public static void main(String[] args) {
+        int valor1 = Integer.parseInt(args[1]);
+        int valor2 = Integer.parseInt(args[2]);
+        System.out.println("Estudiante " + args[0] + " : ");
+        System.out.println(veredicto(valor1,valor2));
+    }
+    static String veredicto (int valor1, int valor2) {
+        if (valor1==(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) > Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+}
